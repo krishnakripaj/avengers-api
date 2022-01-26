2bcbed4a50b8ff42ac8036dd18ecc1a6d675e73e
Kripa Jayakumar <kripa.jay@gmail.com>
2022-01-26 19:07:12 +0530

    Added middleware

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const logger = require("./middleware/logger");
+const authenticator = require("./middleware/authenticator");
 const app = express(); // created an express application
 
-app.use(express.json()); // parse json objects
+app.use(express.json()); // parse json objects ; telling app to use inbuilt middleware
+app.use(logger); // telling app to use the custom middleware logger
+app.use(authenticator);
 
 app.listen(3000, () => {
   console.log("Listening on Port 3000");
@@ -20,6 +24,7 @@ app.get("/", (req, res) => {
 
 // GET ALL
 app.get("/api/avengers", (req, res) => {
+  console.log("GET Method called ....... ");
   res.send(avengerArray);
 });
 
@@ -27,50 +32,50 @@ app.get("/api/avengers", (req, res) => {
 // ==      comparing the value "2" == 2
 // ===     comparing the value and the data type "2" === 2
 app.get("/api/avengers/:avengerId", (req, res) => {
-  let avenger = avengerArray.find(a=>a.id == req.params.avengerId);
+  let avenger = avengerArray.find((a) => a.id == req.params.avengerId);
   if (!avenger) {
-      return res.status(404).send("The given ID does not exist on our system");
+    return res.status(404).send("The given ID does not exist on our system");
   }
   res.status(200).send(avenger);
 });
 
 app.post("/api/avengers", (req, res) => {
-    //Validations
-    if (!req.body.avengerName) {
-        return res.status(400).send("Not all mandatory values are sent");
-    }
+  //Validations
+  if (!req.body.avengerName) {
+    return res.status(400).send("Not all mandatory values are sent");
+  }
 
-    let newAvengerObj = {
-        id: avengerArray.length + 1,
-        name: req.body.avengerName
-    };
-    avengerArray.push(newAvengerObj);
-    res.send(newAvengerObj);
-})
-
-app.put('/api/avengers/:avengerId', (req, res) => {
-    let avenger = avengerArray.find(a=>a.id == req.params.avengerId);
-    if (!avenger) {
-        return res.status(404).send("The given ID does not exist on our system");
-    }
-
-    // validation
-    if (!req.body.avengerName) {
-        return res.status(400).send("Not all mandatory values are sent");
-    }
-
-    avenger.name = req.body.avengerName;
-    res.send(avenger);
+  let newAvengerObj = {
+    id: avengerArray.length + 1,
+    name: req.body.avengerName,
+  };
+  avengerArray.push(newAvengerObj);
+  res.send(newAvengerObj);
 });
 
-app.delete('/api/avengers/:avengerId', (req, res) => {
-    let avenger = avengerArray.find(a=>a.id == req.params.avengerId);
-    if (!avenger) {
-        return res.status(404).send("The given ID does not exist on our system");
-    }
+app.put("/api/avengers/:avengerId", (req, res) => {
+  let avenger = avengerArray.find((a) => a.id == req.params.avengerId);
+  if (!avenger) {
+    return res.status(404).send("The given ID does not exist on our system");
+  }
 
-    let indexOfAvenger = avengerArray.indexOf(avenger);
-    avengerArray.splice(indexOfAvenger, 1);
+  // validation
+  if (!req.body.avengerName) {
+    return res.status(400).send("Not all mandatory values are sent");
+  }
 
-    res.send(avenger);
-})
+  avenger.name = req.body.avengerName;
+  res.send(avenger);
+});
+
+app.delete("/api/avengers/:avengerId", (req, res) => {
+  let avenger = avengerArray.find((a) => a.id == req.params.avengerId);
+  if (!avenger) {
+    return res.status(404).send("The given ID does not exist on our system");
+  }
+
+  let indexOfAvenger = avengerArray.indexOf(avenger);
+  avengerArray.splice(indexOfAvenger, 1);
+
+  res.send(avenger);
+});
